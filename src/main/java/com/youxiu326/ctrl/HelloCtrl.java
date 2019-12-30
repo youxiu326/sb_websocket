7bbd5325f497c5f6a4a62304739f37609fc9db18
youxiu3266 <2473538988@qq.com>
2019-12-30 17:43:58 +0800

    提交布局

--- src/main/java/com/youxiu326/ctrl/HelloCtrl.java
+++ src/main/java/com/youxiu326/ctrl/HelloCtrl.java
@@ -1,12 +1,26 @@
 package com.youxiu326.ctrl;
 
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloCtrl {
 
+    /**
+     * 动态代码布局
+     * @return
+     */
+    @GetMapping("/view")
+    public String view(){
+        return "view";
+    }
+
+    /**
+     * 静态代码布局
+     * @return
+     */
     @GetMapping("/index")
     public String layout(){
         return "index";
@@ -20,6 +34,13 @@ public class HelloCtrl {
     @GetMapping("/")
     public String index(){
         return "websocket";
+    }
+
+    @GetMapping("/menus/data")
+    public JSONPObject menusData(){
+
+
+        return null;
     }
 
     @GetMapping("/byZero")
